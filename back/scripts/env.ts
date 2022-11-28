91588416140b58be19a2992d5ddc001b8c0cea83
dunde <chldlsrb1000@naver.com>
2022-11-28 14:18:11 +0900

    Modify: env file and script

--- back/scripts/env.ts
+++ back/scripts/env.ts
@@ -1,6 +1,7 @@
 import { readFileSync, writeFileSync } from 'fs';
 import * as path from 'path';
 import { TAB } from '@local/shared/dist/const/string';
+import { isEmpty } from '@local/shared/dist/util/validate';
 
 console.info('create env type file...');
 
@@ -9,7 +10,8 @@ const file = readFileSync(path.resolve(__dirname, '../env/.env'), { encoding: 'u
 const envKeys = file
   .split('\n')
   .filter((line) => line[0] !== '#')
-  .map((line) => line.split('=')[0]);
+  .map((line) => line.split('=')[0])
+  .filter((key) => !isEmpty(key));
 
 const data = envKeys.reduce((prev, key) => prev + `${TAB}${key}: string;\n`, 'interface Env {\n') + '}\n';
 
--- back/src/settings/environment.ts
+++ back/src/settings/environment.ts
@@ -1,6 +1,10 @@
 import * as dotenv from 'dotenv';
 
 export const useEnv = () => {
+  const envState = process.env.STATE;
+
+  if (envState === 'on') return;
+
   const isDev = process.env.NODE_ENV === 'dev';
   dotenv.config({ path: `env/${isDev ? '.dev' : ''}.env` });
 };
