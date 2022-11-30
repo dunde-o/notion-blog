b8bfcffd60a2c7d157278e358b12285900f1fefe
dunde <chldlsrb1000@naver.com>
2022-11-30 16:29:01 +0900

    Modify: notion service error logic

--- back/src/services/notions/notions.service.ts
+++ back/src/services/notions/notions.service.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { notionClient, notionSecret } from '@src/utils/notion';
 import { NotionResponseDto } from '@services/notions/dto/notionResponse.dto';
 import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
+import { HttpFailResponseForm } from '@local/shared/dist/types/message/http';
 
 @Injectable()
 export class NotionsService {
@@ -13,7 +14,13 @@ export class NotionsService {
         start_cursor: nextCursor,
       });
     } catch (error: unknown) {
-      throw new HttpException(error, HttpStatus.BAD_REQUEST);
+      throw new HttpException(
+        {
+          error,
+          message: typeof error['code'] === 'string' ? error['code'] : 'unknown error.',
+        } as HttpFailResponseForm['error'],
+        HttpStatus.BAD_REQUEST,
+      );
     }
 
     return new NotionResponseDto(response);
