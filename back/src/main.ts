590c6e40bcf9557681c097dd554f8b252361cf8c
Dunde <chldlsrb1000@naver.com>
2022-11-30 16:52:28 +0900

    [Back] 사용 포트 변경 (#4)

--- back/src/main.ts
+++ back/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new HttpInterceptor());
   setupSwagger(app);
-  await app.listen(3000);
+  await app.listen(8000);
 }
 
 bootstrap();
