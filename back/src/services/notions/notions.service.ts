6c430518627adffa8fe2face3e7cfdc34e383cfc
dunde <chldlsrb1000@naver.com>
2022-11-30 15:36:59 +0900

    Add: notion service error catch

--- back/src/services/notions/notions.service.ts
+++ back/src/services/notions/notions.service.ts
@@ -1,14 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { notionClient, notionSecret } from '@src/utils/notion';
 import { NotionResponseDto } from '@services/notions/dto/notionResponse.dto';
+import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
 
 @Injectable()
 export class NotionsService {
   async getAllNotionPage(nextCursor?: string): Promise<NotionResponseDto> {
-    const response = await notionClient.databases.query({
-      database_id: notionSecret.NOTION_DATABASE_ID,
-      start_cursor: nextCursor,
-    });
+    let response: QueryDatabaseResponse;
+    try {
+      response = await notionClient.databases.query({
+        database_id: notionSecret.NOTION_DATABASE_ID,
+        start_cursor: nextCursor,
+      });
+    } catch (error: unknown) {
+      throw new HttpException(error, HttpStatus.BAD_REQUEST);
+    }
 
     return new NotionResponseDto(response);
   }
