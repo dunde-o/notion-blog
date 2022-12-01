2c3f0a99eda2644c784f8d20ea157ba93e5171b6
dunde <chldlsrb1000@naver.com>
2022-12-01 18:14:44 +0900

    Modify: package script and back eslint

--- back/.eslintrc.js
+++ back/.eslintrc.js
@@ -24,7 +24,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/no-unsafe-argument': 'error',
     '@typescript-eslint/no-unsafe-assignment': 'error',
-    '@typescript-eslint/no-unsafe-call': 'error',
     '@typescript-eslint/no-unsafe-member-access': 'error',
     '@typescript-eslint/no-unsafe-return': 'error',
   },
