51bca61219988e2d70834e2de37839f93fb4acff
dunde <chldlsrb1000@naver.com>
2022-12-14 17:03:38 +0900

    update: path constants and utils

--- front/common/constants/path.ts
+++ front/common/constants/path.ts
@@ -1,16 +1,25 @@
-export const DEFAULT_PATH = '/';
+export const PATH = {
+  MAIN: "/",
+  POSTS: "posts/",
+  PROFILE: "profile/",
+};
+
+export const DEFAULT_PATH = PATH.MAIN;
 
 export const PATH_TO_TITLE = {
-  [DEFAULT_PATH]: 'Dunde\'s blog',
+  [PATH.MAIN]: "Dunde's blog",
 };
 
 export const PATH_TO_NAME = {
-  [DEFAULT_PATH]: 'Main',
-  'NOT_FOUND_PATH': 'Not Found',
+  [PATH.MAIN]: "Main",
+  [PATH.POSTS]: "Posts",
+  [PATH.PROFILE]: "Profile",
+  NOT_FOUND_PATH: "Not Found",
 };
 
 export const PATH_TO_DESCRIPTION = {
-  [DEFAULT_PATH]: 'Dunde\'s blog main page.',
-  'NOT_FOUND_PATH': 'Not found page.',
+  [PATH.MAIN]: "Dunde's blog main page.",
+  [PATH.POSTS]: "Dunde's blog posts page.",
+  [PATH.PROFILE]: "creator profile page.",
+  NOT_FOUND_PATH: "Not found page.",
 };
-
