20a99595fc2c227d5a82ff62cd0f73ca655cb2a5
dunde <chldlsrb1000@naver.com>
2022-12-14 17:02:36 +0900

    Add: components index

---
+++ front/components/atoms/index.ts
@@ -0,0 +1,3 @@
+export { default as Logo } from "./Logo/Logo";
+export { default as RouterButton } from "./RouterButton/RouterButton";
+export { default as TextLink } from "./TextLink/TextLink";
---
+++ front/components/molecules/index.ts
@@ -0,0 +1,2 @@
+export { default as NavLogo } from "./NavLogo/NavLogo";
+export { default as NavMenu } from "./NavMenu/NavMenu";
