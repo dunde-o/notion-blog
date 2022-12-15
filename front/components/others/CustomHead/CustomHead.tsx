89df3debe4b30f65d687fd53c0429d291adb071b
dunde <chldlsrb1000@naver.com>
2022-12-15 15:48:23 +0900

    Fix: CustomHead component by warning msg

--- front/components/others/CustomHead/CustomHead.tsx
+++ front/components/others/CustomHead/CustomHead.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import _Head from 'next/head';
-import { CustomHeadProps } from './CustomHead.type';
-import { DEFAULT_PATH, PATH_TO_TITLE } from '@common/constants/path';
-import { getPathToHeadInfo } from '@common/utils/path';
+import React from "react";
+import _Head from "next/head";
+import { CustomHeadProps } from "./CustomHead.type";
+import { DEFAULT_PATH, PATH_TO_TITLE } from "@common/constants/path";
+import { getPathToHeadInfo } from "@common/utils/path";
 
 const CustomHead: React.FC<CustomHeadProps> = (props) => {
-  const { title = PATH_TO_TITLE[DEFAULT_PATH], path = DEFAULT_PATH, children } = props;
+  const {
+    title = PATH_TO_TITLE[DEFAULT_PATH],
+    path = DEFAULT_PATH,
+    children,
+  } = props;
   const { name, description } = getPathToHeadInfo(path);
-  
-  return <_Head>
-    <title>{name} | {title}</title>
-    <meta name='description' content={description} />
-    {children}
-  </_Head>;
+
+  return (
+    <_Head>
+      <title>{`${name} | ${title}`}</title>
+      <meta name="description" content={description} />
+      {children}
+    </_Head>
+  );
 };
 
-CustomHead.displayName = 'CustomHead';
+CustomHead.displayName = "CustomHead";
 
 export default CustomHead;
