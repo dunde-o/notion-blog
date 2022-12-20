2b940b9408289c0a1ca559f8b3b788d670bea403
dunde <chldlsrb1000@naver.com>
2022-12-20 19:10:25 +0900

    Add: img const file

---
+++ front/common/constants/image.ts
@@ -0,0 +1,7 @@
+export const IMG_SIZE = {
+  ICON: { width: 50, height: 50 },
+  SMALL: { width: 100, height: 100 },
+  MIDDLE: { width: 500, height: 500 },
+  LARGE: { width: 1000, height: 1000 },
+  BACKGROUND: { width: 2000, height: 2000 },
+};
