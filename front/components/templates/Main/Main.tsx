ea8d84b273eee628a92d329abfb492bfdefdf009
dunde <chldlsrb1000@naver.com>
2022-12-21 19:01:23 +0900

    Update: Main component

--- front/components/templates/Main/Main.tsx
+++ front/components/templates/Main/Main.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { MainProps } from "./Main.type";
 import styles from "./Main.module.scss";
 import { Navigator } from "@components/organisms";
+import { Banner } from "@components/atoms";
 
-const Main: React.FC<MainProps> = () => {
+const Main: React.FC<MainProps> = (props) => {
+  const { bannerMessage } = props;
   const { navRef, navPosition, scrollHandler } = Navigator.useNavigatorScroll();
-
   return (
     <main onScroll={scrollHandler} className={styles.Main}>
       <Navigator ref={navRef} position={navPosition} />
+      <Banner message={bannerMessage} />
     </main>
   );
 };
