78698eb29c3740b13f3c8df30037727a917f0f8c
dunde <chldlsrb1000@naver.com>
2022-12-26 01:15:10 +0900

    Modify: storybook setting

--- front/.storybook/main.js
+++ front/.storybook/main.js
@@ -3,7 +3,6 @@ const path = require("path");
 module.exports = {
   stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
     "../components/**/*.stories.mdx",
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
