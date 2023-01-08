309329fc0f7b491bf7c35fbcd7e1e7889bca7f7a
dunde <chldlsrb1000@naver.com>
2023-01-08 22:52:55 +0900

    Add: api default query option

---
+++ front/common/api/const.ts
@@ -0,0 +1,11 @@
+import { UseQueryOptions } from 'react-query';
+
+/**
+ * @description use UseQueryOptions type
+ * @type UseQueryOptions
+ */
+export const QUERY_OPTION = {
+  DEFAULT: {
+    refetchOnWindowFocus: false,
+  },
+};
