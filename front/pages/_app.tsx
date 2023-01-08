37716dfc500d58a2134ee49867cfcd73aa359d99
dunde <chldlsrb1000@naver.com>
2023-01-08 19:49:40 +0900

    Add: react-query lib and app setting

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,14 +1,19 @@
-import '@common/styles/index.scss';
-import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
-import { CustomHead } from '@components/others';
+import "@common/styles/index.scss";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { CustomHead } from "@components/others";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
   const { pathname } = useRouter();
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <CustomHead path={pathname} />
       <Component {...pageProps} />
-    </>
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
   );
 }
