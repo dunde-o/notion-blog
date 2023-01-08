48d6befa39d8351cffe16da103df439c9a9f426f
dunde <chldlsrb1000@naver.com>
2023-01-08 22:53:45 +0900

    Fix: home page

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,15 +1,15 @@
 import { Main } from "@components/templates";
 import { useQuery } from "react-query";
+import { defaultFetch } from "@common/api/util";
+import { NotionPage } from "@local/shared/dist/types/models/notion";
+import { QUERY_OPTION } from "@common/api/const";
 
 export default function Home() {
-  const { isLoading, data } = useQuery("PostPreview", async () => {
-    const response = await fetch("api/posts", { method: "get" });
-    return response.json();
-  });
-
-  if (!isLoading) {
-    console.log(data);
-  }
+  const { isLoading, data } = useQuery(
+    "PostPreview",
+    () => defaultFetch<NotionPage[]>("api/posts", "GET"),
+    QUERY_OPTION.DEFAULT
+  );
 
   return (
     <Main
