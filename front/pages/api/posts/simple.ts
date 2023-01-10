00b5fac9910788e4d1d6a5c37a8ef9ef0ded8681
dunde <chldlsrb1000@naver.com>
2023-01-11 06:41:24 +0900

    Add: api posts/simple file

---
+++ front/pages/api/posts/simple.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { NotionPages } from "@local/shared/dist/types/models/notion";
+import { ExecutionType } from "@common/api/type";
+import { executeHttpMethod } from "@common/api/util";
+import { serverFetch } from "@common/utils/message";
+
+const execution: ExecutionType = {
+  GET: async (req, res) => {
+    const response = await serverFetch<NotionPages>(
+      { url: "/notions", isServer: true },
+      "GET"
+    );
+
+    res
+      .status(response.success ? 200 : Number(response.error.statusCode))
+      .json(response.success ? response.data : response.error.message);
+  },
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<NotionPages>
+) {
+  executeHttpMethod(req, res, execution);
+}
