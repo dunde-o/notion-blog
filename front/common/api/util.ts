91e5ca124e2e9e1e62283d0dbc1890f690bb3476
dunde <chldlsrb1000@naver.com>
2023-01-11 06:38:48 +0900

    Modify: api util file + Add: message util file

--- front/common/api/util.ts
+++ front/common/api/util.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { HttpMethod } from "@local/shared/dist/types/message/http";
-import { ExecutionType, requestType } from "./type";
-import { isEmpty } from "@local/shared/dist/util/validate";
+import { ExecutionType } from "./type";
 
 export const executeHttpMethod = (
   req: NextApiRequest,
@@ -12,41 +11,3 @@ export const executeHttpMethod = (
   if (method == null || execution[method] == null) return;
   execution[method]?.(req, res);
 };
-
-export const objectToQueryString = (queryObject?: requestType): string => {
-  if (queryObject == null) return "";
-  const query = Object.entries(queryObject)
-    .reduce<string[]>((arr, [key, value]) => [...arr, `${key}=${value}`], [])
-    .join("&");
-  return isEmpty(query) ? "" : `?${query}`;
-};
-
-async function defaultFetch<T>(
-  url: string,
-  method: "GET",
-  requestData?: { param?: requestType }
-): Promise<T | undefined>;
-async function defaultFetch<T>(
-  url: string,
-  method: "POST" | "PUT" | "PATCH" | "DELETE",
-  requestData?: { body?: requestType }
-): Promise<T | undefined>;
-async function defaultFetch<T>(
-  url: string,
-  method: HttpMethod,
-  requestData?: { param?: requestType; body?: requestType }
-): Promise<T | undefined> {
-  const fetchOptions: RequestInit = { method };
-
-  if (method === "GET" && requestData?.param != null) {
-    const query = objectToQueryString(requestData.param);
-    url = `${url}${query}`;
-  } else if (requestData?.body != null) {
-    fetchOptions.body = JSON.stringify(requestData.body);
-  }
-
-  const response = await fetch(url, fetchOptions);
-  return response.json();
-}
-
-export { defaultFetch };
---
+++ front/common/utils/message.ts
@@ -0,0 +1,106 @@
+import { RequestType, UrlType } from "@common/api/type";
+import { isEmpty } from "@local/shared/dist/util/validate";
+import {
+  HttpMethod,
+  HttpResponseForm,
+} from "@local/shared/dist/types/message/http";
+
+export const objectToQueryString = (queryObject?: RequestType): string => {
+  if (queryObject == null) return "";
+  const query = Object.entries(queryObject)
+    .reduce<string[]>((arr, [key, value]) => [...arr, `${key}=${value}`], [])
+    .join("&");
+  return isEmpty(query) ? "" : `?${query}`;
+};
+
+async function serverFetch<T>(
+  url: UrlType,
+  method: "GET",
+  requestData?: { param?: RequestType }
+): Promise<HttpResponseForm<T>>;
+async function serverFetch<T>(
+  url: UrlType,
+  method: "POST" | "PUT" | "PATCH" | "DELETE",
+  requestData?: { body?: RequestType }
+): Promise<HttpResponseForm<T>>;
+async function serverFetch<T>(
+  url: UrlType,
+  method: HttpMethod,
+  requestData?: { param?: RequestType; body?: RequestType }
+): Promise<HttpResponseForm<T>> {
+  const fetchOptions: RequestInit = { method };
+  const { isServer } = typeof url === "string" ? { isServer: false } : url;
+
+  if (typeof url !== "string") {
+    url =
+      (isServer
+        ? `${process.env.SERVER_SECURE ? "https://" : "http://"}` +
+          `${process.env.SERVER_URL}`
+        : "") + url.url;
+  }
+
+  if (method === "GET" && requestData?.param != null) {
+    const query = objectToQueryString(requestData.param);
+    url = `${url}${query}`;
+  } else if (requestData?.body != null) {
+    fetchOptions.body = JSON.stringify(requestData.body);
+  }
+
+  try {
+    const response = await fetch(url, fetchOptions);
+    const data = await response.json();
+    return isServer
+      ? data
+      : {
+          success: true,
+          path: url,
+          timeStamp: new Date(),
+          data,
+        };
+  } catch (error) {
+    return {
+      success: false,
+      path: url,
+      timeStamp: new Date(),
+      error: {
+        message: error as string,
+        statusCode: "400",
+        ...(error as { [key in string]: string }),
+      },
+    };
+  }
+}
+
+async function clientFetch<T>(
+  url: string,
+  method: "GET",
+  requestData?: { param?: RequestType }
+): Promise<T | undefined>;
+async function clientFetch<T>(
+  url: string,
+  method: "POST" | "PUT" | "PATCH" | "DELETE",
+  requestData?: { body?: RequestType }
+): Promise<T | undefined>;
+async function clientFetch<T>(
+  url: string,
+  method: HttpMethod,
+  requestData?: { param?: RequestType; body?: RequestType }
+): Promise<T | undefined> {
+  const fetchOptions: RequestInit = { method };
+
+  if (method === "GET" && requestData?.param != null) {
+    const query = objectToQueryString(requestData.param);
+    url = `${url}${query}`;
+  } else if (requestData?.body != null) {
+    fetchOptions.body = JSON.stringify(requestData.body);
+  }
+
+  try {
+    const response = await fetch(url, fetchOptions);
+    return response.json();
+  } catch (error) {
+    return;
+  }
+}
+
+export { serverFetch, clientFetch };
