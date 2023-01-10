0de08596ab7f60d8424f8d130ff59de8f2581749
dunde <chldlsrb1000@naver.com>
2023-01-11 06:42:12 +0900

    Modify: home page

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,20 +1,20 @@
 import { Main } from "@components/templates";
 import { useQuery } from "react-query";
-import { defaultFetch } from "@common/api/util";
-import { NotionPage } from "@local/shared/dist/types/models/notion";
+import { NotionPages } from "@local/shared/dist/types/models/notion";
 import { QUERY_OPTION } from "@common/api/const";
+import { clientFetch } from "@common/utils/message";
 
 export default function Home() {
   const { isLoading, data } = useQuery(
     "PostPreview",
-    () => defaultFetch<NotionPage[]>("api/posts", "GET"),
+    () => clientFetch<NotionPages>("api/posts/simple", "GET"),
     QUERY_OPTION.DEFAULT
   );
 
   return (
     <Main
       bannerMessage={"Dunde's notion blog"}
-      postData={data}
+      postData={data?.results}
       isLoading={isLoading}
     />
   );
