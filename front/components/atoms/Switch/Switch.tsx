e8a2414d76f010e025bb71e45a818b13ec688886
dunde <chldlsrb1000@naver.com>
2023-02-02 16:34:36 +0900

    Add: Switch component + Add: DefaultSwitch component

---
+++ front/components/atoms/Switch/Switch.tsx
@@ -0,0 +1,38 @@
+import React, { useCallback, useState } from "react";
+import styles from "./Switch.module.scss";
+import { SwitchProps } from "./Switch.type";
+import DefaultSwitch from "./shared/DefaultSwitch";
+
+const Switch: React.FC<SwitchProps> = (props) => {
+  const {
+    children,
+    defaultSwitchProps,
+    defaultAction,
+    otherAction,
+    commonAction,
+    ...rest
+  } = props;
+  const [switchState, setSwitchState] = useState(
+    defaultSwitchProps?.state ?? true
+  );
+
+  const handleButtonClick = useCallback(() => {
+    switchState ? otherAction?.() : defaultAction?.();
+    commonAction?.();
+    setSwitchState((state) => !state);
+  }, [switchState, defaultAction, otherAction, commonAction]);
+
+  return (
+    <button className={styles.Switch} {...rest} onClick={handleButtonClick}>
+      {defaultSwitchProps == null ? (
+        children
+      ) : (
+        <DefaultSwitch state={switchState} icon={defaultSwitchProps.icon} />
+      )}
+    </button>
+  );
+};
+
+Switch.displayName = "Switch";
+
+export default Switch;
