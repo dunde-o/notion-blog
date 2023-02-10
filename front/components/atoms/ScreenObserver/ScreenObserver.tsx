0523c3d0b4514646ecfcd0364767b31b3cdc316a
dunde <chldlsrb1000@naver.com>
2023-02-10 23:20:35 +0900

    Add: ScreenObserver component

---
+++ front/components/atoms/ScreenObserver/ScreenObserver.tsx
@@ -0,0 +1,40 @@
+import React, { createRef, useLayoutEffect } from "react";
+import { ScreenObserverProps } from "./ScreenObserver.type";
+
+const ScreenObserver: React.FC<ScreenObserverProps> = (props) => {
+  const { onInScreen, onOutScreen, isLoading = false } = props;
+  const ref = createRef<HTMLDivElement>();
+
+  useLayoutEffect(() => {
+    let observer: IntersectionObserver;
+    if (ref.current != null) {
+      observer = new IntersectionObserver(
+        async (entries, _observer) => {
+          if (isLoading) {
+            return;
+          }
+          const [entry] = entries;
+          _observer.unobserve(entry.target);
+          if (entry.isIntersecting) {
+            await onInScreen?.();
+          } else {
+            await onOutScreen?.();
+          }
+          _observer.observe(entry.target);
+        },
+        { threshold: 1 }
+      );
+      observer.observe(ref.current);
+    }
+    return () => {
+      if (observer != null) {
+        observer.disconnect();
+      }
+    };
+  }, [ref, isLoading, onInScreen, onOutScreen]);
+  return <div ref={ref} />;
+};
+
+ScreenObserver.displayName = "ScreenObserver";
+
+export default ScreenObserver;
