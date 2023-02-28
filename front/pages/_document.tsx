e7e67400bcb1fcb72e8817a718b6bee259ff5c09
Dunde <chldlsrb1000@naver.com>
2023-02-28 23:30:14 +0900

    [Release] 포스트카드 이미지 개선, 문서 속성 추가 (v0.3.3) (#26)

---
+++ front/pages/_document.tsx
@@ -0,0 +1,14 @@
+// pages/_document.js
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
