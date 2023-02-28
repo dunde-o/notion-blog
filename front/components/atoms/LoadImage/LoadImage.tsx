fc4049c92e113b9b442e2a16952dde1bf6871632
Dunde <chldlsrb1000@naver.com>
2023-02-28 23:26:27 +0900

    [Front] 포스트 카드 이미지 개선 및 문서 속성 추가 (#25)

--- front/components/atoms/LoadImage/LoadImage.tsx
+++ front/components/atoms/LoadImage/LoadImage.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { LoadImageProps } from "./LoadImage.type";
-import { IMG_PROPS } from "@common/constants/image";
+import styles from "./LoadImage.module.scss";
+import classNames from "classnames";
 
 const LoadingImage: React.FC<LoadImageProps> = (props) => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   return (
     // eslint-disable-next-line jsx-a11y/alt-text
-    <Image {...props} placeholder="blur" blurDataURL={IMG_PROPS.LOADING.src} />
+    <Image
+      {...props}
+      className={classNames(props.className, {
+        [styles["is-loading"]]: isLoading,
+      })}
+      onLoadingComplete={() => setIsLoading(false)}
+    />
   );
 };
 
